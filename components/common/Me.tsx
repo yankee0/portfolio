0878cd3183af0a393936dce103c39bef75a34c7d
yankee0 <yankeesuprem@gmail.com>
2025-11-08 15:32:49 +0000

    basic scale animation on links

--- components/common/Me.tsx
+++ components/common/Me.tsx
@@ -12,8 +12,10 @@ export type SocialMedia = {
 
 export default function Me({ imgSrc, title, socialMediaIcon, username, url }: SocialMedia) {
   return (
-    <Link href={url} className='flex gap-4 items-center'>
-      <Image src={imgSrc} alt={title} height={80} width={80} className='aspect-square h-20 w-20' />
+    <Link href={url} className='flex gap-4 items-center group'>
+      <div className='aspect-square h-20 w-20 overflow-clip'>
+      <Image src={imgSrc} alt={title} height={80} width={80} className='aspect-square h-20 w-20 group-hover:scale-110 transition-all duration-300' />
+      </div>
       <div className='space-y-2'>
         <h2 className='font-bold text-[16px]/[120%]'>{title}</h2>
         <div className='flex gap-0 items-center'>
